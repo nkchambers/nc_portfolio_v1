868b55d496e3879b31ad09e768e349d4789c82a6
nkchambers <nkchames@gmail.com>
2023-12-27 19:28:19 -0800

    added engines to pckge.json

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // server used to send send emails
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json(), express.urlencoded({ extended: true }));
@@ -19,12 +20,6 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' })
 });
-
-
-app.listen(PORT, () => {
-    console.log(`***** Server is running on Port ${PORT} *****`)
-});
-console.log(process.env.EMAIL_ADDRESS);
 
 
 
@@ -67,3 +62,14 @@ app.post("/api/contact", bodyParser.urlencoded({ extended: false }), (req, res) 
         }
     });
 });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+})
+
+
+app.listen(PORT, () => {
+    console.log(`***** Server is running on Port ${PORT} *****`)
+});
+console.log(process.env.EMAIL_ADDRESS);
